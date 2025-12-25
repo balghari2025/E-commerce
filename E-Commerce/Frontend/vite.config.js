58728b88af5f9ed291d676cfb69b7f55b81a9473
balghari2025 <BalghariF069@gmail.com>
2025-12-25 16:01:56 +0500

    update vite congfig

--- E-Commerce/Frontend/vite.config.js
+++ E-Commerce/Frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './https://github.com/balghari2025/E-commerce.git/',
+  base: '/',
   plugins: [react()],
 })
